a035065a030e9349bdf9071c10971884b8aad281
Divino J Silva <divino_j_silva@hotmail.com>
2020-12-30 14:40:45 -0300

    rotas e comandos

--- src/index.js
+++ src/index.js
@@ -3,8 +3,39 @@ const express = require('express');
 
 const app = express();
 
+
 app.get('/projects',(request, response) => {
-    return response.json({message: 'Hello World 2'});
+    const {title, owner} = request.query;
+
+    console.log(title);
+    console.log(owner);
+
+    
+    return response.json([
+
+    ]);
+});
+
+app.post('/projects',(request, response) => {
+    return response.json([
+
+    ]);
+});
+
+app.put('/projects/:id',(request, response) => {
+    const params =request.params;
+
+    console.log(params);
+    
+    return response.json([
+        
+    ]);
+});
+
+app.delete('/projects/:id   ',(request, response) => {
+    return response.json([
+        
+    ]);
 });
 
 app.listen(3333, ()=> {
